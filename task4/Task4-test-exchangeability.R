061d1cb5585eaf77b471a19fce23e7ff49848c53
Léo Belzile <belzilel@gmail.com>
2024-06-26 18:58:17 -0400

    Update Task4-test-exchangeability.R
    Use Sam Perreault released code

--- task4/Task4-test-exchangeability.R
+++ task4/Task4-test-exchangeability.R
@@ -1,7 +1,35 @@
 setwd(this.path::here())
-remotes::install_github("samperochkin/tautest")
-library(tautests)
 
+## This code contains functions from Sam Perreault
+## Sadly, some of the code is in a private Github repository
+# remotes::install_github("samperochkin/tautest")
+# library(tautests)
+# New repository samperochkin/taujack contains other functions
+# But not in a R package, so need some hack to get the code
+## Functions not found on Github repository 'taujack'
+r2ij <- function(r){
+   j <- ceiling((1 + sqrt(1 + 8*r))/2)
+   i <- r - choose(j-1,2)
+
+   return(c(i,j))
+}
+
+R2IJ <- function(R){
+   return(t(sapply(R, r2ij)))
+}
+
+# Other functions by Sam Perreault on Github (but not in a package!)
+
+
+loadCpp <- function(remurl){
+   locfile <- "/tmp/file.cpp"
+   download.file(url=remurl, destfile=locfile)
+   Rcpp::sourceCpp(locfile)
+}
+loadCpp("https://raw.githubusercontent.com/samperochkin/taujack/main/src/ms.cpp")
+source("https://raw.githubusercontent.com/samperochkin/taujack/main/functions.R")
+
+# Load data and reorder
 UtopulaU1_data <- read.csv(file = "../data/UtopulaU1.csv", header = TRUE)
 colnames(UtopulaU1_data) <- paste0("I1_", c(paste0(0, 1:9), 10:25))
 UtopulaU2_data <- read.csv(file = "../data/UtopulaU2.csv", header = TRUE)
@@ -37,44 +65,44 @@ constrainSigma <- function(Sh, clus, debug = FALSE) {
    d <- length(clus)
    p <- d * (d - 1) / 2
    K <- length(unique(clus))
-   
+
    #### vectorizing ####
-   
+
    ij <- tautests::R2IJ(1:p)
    kl <- matrix(clus[c(ij)], p, 2) |> apply(1, sort) |> t()
    kl0 <- unique(kl)
    L <- nrow(kl0)
-   
+
    s_kl <- kl[, 1] + (K + 1) * kl[, 2]
    s_kl0 <- unique(s_kl) # should be in same order as u_kl
    s_kl <- match(s_kl, s_kl0)
    if (length(s_kl0) != nrow(kl0))
       cat("problem in generating unique block ids\n")
-   
+
    # loop to make sure we don't run into memory problems
    for (u in 1:L) {
       for (v in u:L) {
          kl_u <- kl0[u, ]
          kl_v <- kl0[v, ]
-         
+
          if (debug)
             cat("----------\n case", kl_u, "and", kl_v, "\n")
-         
+
          r_u <- which(s_kl == u)
          s_v <- which(s_kl == v)
-         
+
          if (length(intersect(kl_u, kl_v)) == 0) {
             # no overlap possible
             Sh[r_u, s_v] <- Sh[s_v, r_u] <- mean(Sh[r_u, s_v])
             next
          }
-         
+
          # collect entries from both blocks
          ij_u <- ij[r_u, ]
          ij_v <- ij[s_v, ]
          p_u <- nrow(ij_u)
          p_v <- nrow(ij_v)
-         
+
          psi <- mapply(
             FUN = function(x, y) {
                psi <- clus[intersect(ij_u[x, ], ij_v[y, ])]
@@ -84,10 +112,10 @@ constrainSigma <- function(Sh, clus, debug = FALSE) {
             y = rep(1:p_v, each = p_u)
          ) |>
             t() |> c() |> array(dim = c(p_u, p_v, 2))
-         
+
          s_psi <- psi[, , 1] + (L + 1) * psi[, , 2]
          groups <- split(1:length(s_psi), s_psi)
-         
+
          for (g in groups) {
             s <- ceiling(g / p_u)
             r <- g - (s - 1) * p_u
@@ -95,12 +123,12 @@ constrainSigma <- function(Sh, clus, debug = FALSE) {
                Sh[cbind(s_v[s], r_u[r])] <- mean(Sh[cbind(r_u[r], s_v[s])])
             # image(t(Sh)[rev(r_u),s_v])
          }
-         
+
          if (debug) {
             l1 <- min(length(unique(kl_u)), length(unique(kl_v)))
             l2 <- max(length(unique(kl_u)), length(unique(kl_v)))
             l12 <- length(intersect(kl_u, kl_v))
-            
+
             category <- which(
                c(
                   l1 == 1 & l2 == 1 & l12 == 1,
@@ -118,21 +146,27 @@ constrainSigma <- function(Sh, clus, debug = FALSE) {
                 length(unique(c(Sh[r_u, s_v]))) - c(3, 2, 1, 4, 1, 2, 1)[category],
                 "****\n")
          }
-         
+
       }
    }
-   
+
    return(Sh)
 }
 
 
 
 # compute tau and its jackknife variance in O(n*log(n))
-TSh <- tautests::tau_and_jack(X)
-Sh <- TSh$jack_var
-Th <- TSh$tau
+# TSh <- tautests::tau_and_jack(X)
+# Sh <- TSh$jack_var
+# Th <- TSh$tau
+TSh <- taujack_ms(X)
+# TSh$Th contains vectorized Kendall tau
+Th <- pcaPP::cor.fk(X)
+Sh <- TSh$Sh
+
+
 image(t(Th)[, d:1])
-th <- Th[tautests::R2IJ(1:p)]
+th <- Th[R2IJ(1:p)]
 
 hc <- hclust(as.dist(1 - abs(Th)), "ward.D2")
 plot(hc)
@@ -153,7 +187,7 @@ length(unique(c(St))) == (K ^ 4 + 6 * K ^ 3 + 11 * K ^ 2 + 6 * K) / 8
 
 
 # construct membership matrix B
-ij <- tautests::R2IJ(1:p)
+ij <- R2IJ(1:p)
 A <- matrix(0, d, K)
 A[cbind(1:d, clus)] <- 1
 A <- t(apply(ij, 1, function(x)
@@ -198,7 +232,8 @@ abline(v = loss_E,
        col = 2,
        lty = 3,
        lwd = 2)
-# c(pval_E, mean(loss_E <= mc_loss_E)) # just for checking
+# p-values (asymptotic and Monte Carlo)
+c(pval_E, mean(loss_E <= mc_loss_E))
 
 loss_M <- sqrt(n) * max(abs(Sti2 %*% (th - tt)))
 mc_loss_M <- apply(mc_z, 1, function(x)
@@ -209,14 +244,13 @@ abline(v = loss_M,
        col = 2,
        lty = 3,
        lwd = 2)
-pval_M
-
+# Report p-values for two statistics
 c(pval_E, pval_M)
 
 # Case for the I/n as scaling factor
 St_dag <- (IG %*% St %*% IG)
 
-# p-values
+# p-values via Monte Carlo
 mc_z <- mvtnorm::rmvnorm(1e4, sigma = St_dag)
 
 loss_E <- n * crossprod(th - tt)[1, 1]
